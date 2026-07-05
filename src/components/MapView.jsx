32598ceac249b777b42f7988fe80796ecc6a6ef9
Ishaan Shukla <ishaanshukla0317@gmail.com>
2026-07-05 14:52:24 +0530

    Replace Leaflet default marker with Lucide custom marker

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,6 +1,22 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-
+import L from "leaflet";
+import { renderToString } from "react-dom/server";
+import { MapPin } from "lucide-react";
+export const customMarker = L.divIcon({
+  html: renderToString(
+    <MapPin
+      size={40}
+      color="#2563eb"
+      fill="#2563eb"
+      strokeWidth={2}
+    />
+  ),
+  className: "",
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -40],
+});
 const MapView = ({ places }) => {
   return (
     <MapContainer
@@ -21,22 +37,26 @@ const MapView = ({ places }) => {
 
         return (
           <Marker
-            key={index}
-            position={[
-              place.coordinates.latitude,
-              place.coordinates.longitude
-            ]}
-          >
-            <Popup>
-              <h3>{place.name}</h3>
-              <p>{place.description}</p>
-              
-              
-              {place?.photos?.[0]?.url && (
-                 <img src={place.photos[0].url} width="150" alt={place.name} />
-              )}
-            </Popup>
-          </Marker>
+  key={index}
+  position={[
+    place.coordinates.latitude,
+    place.coordinates.longitude,
+  ]}
+  icon={customMarker}
+>
+  <Popup>
+    <h3>{place.name}</h3>
+    <p>{place.description}</p>
+
+    {place?.photos?.[0]?.url && (
+      <img
+        src={place.photos[0].url}
+        width="150"
+        alt={place.name}
+      />
+    )}
+  </Popup>
+</Marker>
         );
       })}
     </MapContainer>
